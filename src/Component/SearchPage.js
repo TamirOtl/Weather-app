2a72ab5a9d8c6edee5cfa711f9e284a522fd379c
Tamir Oberlander <tamir.otl2@gmail.com>
2020-04-21 15:12:14 +0300

    scroll responsive, timezone hourly

--- src/Component/SearchPage.js
+++ src/Component/SearchPage.js
@@ -8,10 +8,12 @@ const api={
     base:"https://api.openweathermap.org/data/2.5/weather"
 }
 
-const SearchPage = () => {
+const SearchPage  = () => {
     const[query,setQuery]=useState('');
     const[weather,setWeather]=useState('');
     const[full,setFull]=useState('')
+    const[Latitude,setLatitude]=useState('')
+    const[longitude,setlongitude]=useState('')
     const search = (e) => {
         e.preventDefault();
           fetch(`${api.base}?q=${query}&appid=${api.key}`)
@@ -26,27 +28,60 @@ const SearchPage = () => {
                 setFull(result2);
                 setQuery('');
                 console.log(result2);
-              }).catch(("Somthing go wrong"));
+              })
               ;
-            }).catch(("Somthing go wrong"));
+            })
             
            
         
       }
+   
 
+      function getLocation() {
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(showPosition);
+        } else { 
+         alert("Geolocation is not supported by this browser.");
+        }
+      }
+      
+      function showPosition(position) {
+        const Latitude= position.coords.latitude ;
+        const longitude= position.coords.longitude;
+        setLatitude(Latitude.toFixed(2));
+        setlongitude(longitude.toFixed(2));
+        ShowWeather();
+      }   
+      function ShowWeather(){
+       
+         fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${Latitude}}&lon=${longitude}&appid=26b12a44f4c02b6690ba0c54d93b86d5`)
+         .then(res2 => res2.json())
+         .then(result2 => {
+           setFull(result2);
+           setQuery('');
+           console.log(result2);
+         }).catch(Error => alert('Somthing go wrong'));
+         
+ 
+       }
   return (
 
     <div >
       		<form  onSubmit={search}> 
 						<input type="text" placeholder="Find your location..."
-                         value={query} placeholder="Search" aria-label="Search" 
+                         value={query}  aria-label="Search" 
                          onChange={e => setQuery(e.target.value)}
                          className="search" autoComplete='true'
                         />
-             <button ><FaSistrix className="fas fa-search" onKeyPress={search}/></button>
+                       
+             <button ><FaSistrix className="fas fa-search" onKeyPress={search} /></button>
+             
 					</form>
-    
-      <WeatherPage weather={weather} full={full} />
+          
+          <button onClick={getLocation}>location</button>
+        
+      <WeatherPage  weather={weather} full={full} />
+      
     </div>
   );
 }
